603de970ac456fe8aa73306e10d2e1f85fa451be
yahomi-jp <estone111223@gmail.com>
2021-09-04 15:47:10 +0900

    add polls.admin.register

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'django_extentions',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.base import Model
 from django.db.models.fields import IntegerField
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -8,8 +10,17 @@ class Question(models.Model):
     question_text = models.CharField('内容', max_length=200)
     pub_date = models.DateTimeField('公開日')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField('内容', max_length=200)
     votes = IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
