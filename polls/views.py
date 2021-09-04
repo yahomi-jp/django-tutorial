2350ade03db0ff0625b185cf3b29ab6617969148
yahomi-jp <estone111223@gmail.com>
2021-09-04 16:22:37 +0900

    create index.hrml

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
