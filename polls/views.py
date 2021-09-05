77fcd1b0159da17022a6cb08fd26c3760dd5b75b
yahomi-jp <estone111223@gmail.com>
2021-09-06 03:40:12 +0900

    replace views def->class

--- polls/views.py
+++ polls/views.py
@@ -3,26 +3,25 @@ from polls.models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404
 from django.urls import reverse
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class ModelListView(ListView):
+    template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/result.html', {'question': question})
+class DetailView(DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = "polls/result.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
